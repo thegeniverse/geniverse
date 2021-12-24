a91bfefc8e8f1401e2cf3deb34ceeb8362d61d80
vipermu <vipermu97@gmail.com>
2021-12-24 17:28:42 +0100

    ch: update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="geniverse",
-    version="0.1.2",
+    version="0.1.3",
     author="Geniverse",
     author_email="vipermu97@gmail.com",
     description=
